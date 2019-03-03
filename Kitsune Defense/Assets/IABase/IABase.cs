a87bff04c4be045ed5fab6ba7465995f43eb894a
Baccarat <yan_sao.paulo@hotmail.com>
2019-03-03 16:37:25 -0300

    cena de testes, e inicio da Ai

---
+++ Kitsune Defense/Assets/IABase/IABase.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IABase : MonoBehaviour
+{
+    public GameObject Objective;
+
+    private NavMeshAgent NavAgent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NavAgent = gameObject.GetComponent<NavMeshAgent>();
+        Objective = GameObject.FindGameObjectWithTag("Objective");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NavAgent.SetDestination(Objective.transform.position);
+    }
+}
---
+++ Kitsune Defense/Assets/IABase/SpawnControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnControl : MonoBehaviour
+{
+    public GameObject MassSpawn;
+    public float Time;
+    
+    void Start()
+    {
+        StartCoroutine(SpawnCorroutine());
+    }
+
+    IEnumerator SpawnCorroutine()
+    {
+        Instantiate(MassSpawn, transform);
+        yield return new WaitForSeconds(Time);
+        StartCoroutine(SpawnCorroutine());
+    }
+}
