58be1d49dd866a69b39a03f8d464642a16a63f28
Baccarat <yan_sao.paulo@hotmail.com>
2019-03-04 19:02:58 -0300

    animação de ataque

--- Kitsune Defense/Assets/IABase/IABase.cs
+++ Kitsune Defense/Assets/IABase/IABase.cs
@@ -6,13 +6,16 @@ using UnityEngine.AI;
 public class IABase : MonoBehaviour
 {
     public GameObject Objective;
+    public float dToAttack;
 
     private NavMeshAgent NavAgent;
-
+    private Animator Anim;
+    
     // Start is called before the first frame update
     void Start()
     {
         NavAgent = gameObject.GetComponent<NavMeshAgent>();
+        Anim = gameObject.GetComponent<Animator>();
         Objective = GameObject.FindGameObjectWithTag("Objective");
     }
 
@@ -20,5 +23,25 @@ public class IABase : MonoBehaviour
     void Update()
     {
         NavAgent.SetDestination(Objective.transform.position);
+        if (NavAgent.velocity.sqrMagnitude > 0)
+        {
+            Anim.SetBool("Moving", true);
+        }else
+        {
+            Anim.SetBool("Moving", false);
+        }
+
+        float Distance = Vector3.Distance(transform.position, Objective.transform.position);
+        if (Distance <= dToAttack)
+        {
+            Debug.Log("Entro");
+            Anim.SetBool("Attacking", true);
+            NavAgent.isStopped = true;
+        }
+        else
+        {
+            Anim.SetBool("Attacking", false);
+            NavAgent.isStopped = false;
+        }
     }
 }
