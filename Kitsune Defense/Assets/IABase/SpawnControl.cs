d55144dac279e92a7f38c35b882175aa4570545d
Unknown <yan_sao.paulo@hotmail.com>
2019-03-31 19:45:29 -0300

    ajuste na navmesh da faze, spawnando arqueiro e Samurai(só o slot),  mudando lugar do spawn,

--- Kitsune Defense/Assets/IABase/SpawnControl.cs
+++ Kitsune Defense/Assets/IABase/SpawnControl.cs
@@ -5,19 +5,24 @@ using UnityEngine;
 public class SpawnControl : MonoBehaviour
 {
     public GameObject LancerEnemy;
-    public GameObject[] SpawnHoots;
+    public GameObject ArcherEnemy;
+    public GameObject[] SpawnHootsList;
+    GameObject SpawnHoot;
     float Time;
     int MaxWavesNumber = 3;
     static public int KilledEnemies;
     int CurrentWave = 1;
     int MaxEnemysNumber;
     int CurrentEnemyNumber;
+    int SpawIndex = 0;
 
     public GameUIManager gameUIManager;
 
     void Start()
     {
+        SpawnHoot = SpawnHootsList[0];
         gameUIManager = GameObject.FindGameObjectWithTag("UIManager").GetComponent<GameUIManager>();
+        KilledEnemies = 0;
         StartCoroutine(SpawnCorroutine());
     }
     private void Update()
@@ -27,19 +32,23 @@ public class SpawnControl : MonoBehaviour
             case 1:
                 MaxEnemysNumber = 2;
                 Time = 5;
+                SpawnHoot = SpawnHootsList[0];
                 break;
             case 2:
                 MaxEnemysNumber = 5;
                 Time = 3;
+                SpawnHoot = SpawnHootsList[1];
                 break;
             case 3:
                 MaxEnemysNumber = 10;
                 Time = 2;
+                SpawnHoot = SpawnHootsList[0];
                 break;
         }
         if (KilledEnemies == MaxEnemysNumber)
         {
             CurrentWave++;
+            SpawIndex = 0;
             KilledEnemies = 0;
             CurrentEnemyNumber = 0;
             if (CurrentWave <= MaxWavesNumber)
@@ -52,9 +61,24 @@ public class SpawnControl : MonoBehaviour
             }
         }     
     }
-    IEnumerator SpawnCorroutine()
+    public IEnumerator SpawnCorroutine()
     {
-        Instantiate(LancerEnemy, SpawnHoots[0].transform);
+        GameObject Enemy;
+        if (SpawIndex <=2)
+        {
+            Enemy = Instantiate(LancerEnemy, SpawnHoot.transform.position, SpawnHoot.transform.rotation) as GameObject;
+            SpawIndex ++;
+        }
+        else if (SpawIndex <=4)
+        {
+            Enemy = Instantiate(ArcherEnemy, SpawnHoot.transform.position, SpawnHoot.transform.rotation) as GameObject;
+            SpawIndex ++;
+        }
+        else if (SpawIndex == 5)
+        {
+            Enemy = Instantiate(ArcherEnemy, SpawnHoot.transform.position, SpawnHoot.transform.rotation) as GameObject;
+            SpawIndex = 0;
+        }
         CurrentEnemyNumber++;
         yield return new WaitForSeconds(Time);
         if (CurrentEnemyNumber < MaxEnemysNumber)
