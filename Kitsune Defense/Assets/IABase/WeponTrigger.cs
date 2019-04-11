7e6678fa029c258f942edd81fa940acfb8714210
Unknown <yan_sao.paulo@hotmail.com>
2019-04-11 19:51:34 -0300

    Algumas correções na state machine, combate funcionando com collider

---
+++ Kitsune Defense/Assets/IABase/WeponTrigger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeponTrigger : MonoBehaviour
+{
+    IAMelee Reference;
+
+    void Start()
+    {
+        Reference = transform.GetComponentInParent<IAMelee>();
+    }
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") || other.CompareTag("Objective"))
+        {
+            Reference.DealDamage();
+        }
+    }
+}
